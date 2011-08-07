e052782dcb7030d29ff111ed3400f4a9577ae7f9
Joshua T Kalis <kalisjoshua@gmail.com>
2011-08-07 10:40:49 -0400

    xhr requests working in FF,SF,IE; loading content using innerHTML working as well

--- js/intercept.js
+++ js/intercept.js
@@ -4,6 +4,7 @@
 var Intercept = (function () {
   var
     _ = function (id) {
+
       return document.getElementById(id);
     },
 
@@ -17,7 +18,9 @@ var Intercept = (function () {
     config = {},
     
     dropEvent = (function (handler) {
-      var on = handler === "detachEvent" ? "on" : "";
+      var
+        on = handler === "detachEvent" ? "on" : "";
+
       return function (element, eventType, fn) {
         element[handler](on + eventType, fn, false);
       };
@@ -28,27 +31,61 @@ var Intercept = (function () {
         indx = 0,
         nodes = parent.childNodes,
         regex = new RegExp(child, "i");
-      while (!regex.test(nodes[indx].nodeName) && indx++ < nodes.length);
+
+      while (!regex.test(nodes[indx].nodeName) && indx++ < nodes.length) {}
+
       return nodes[indx];
     },
     
     localLinks = function (e) {
       e = normalizeEvent(e);
-      if (e.target.href !== "" && window.location.host === e.target.host) {
+
+      // for some reason IE adds a port number to the end of host
+      if (e.target.href !== "" && window.location.host === e.target.host.replace(/:.*/, "")) {
         e.preventDefault ? e.preventDefault() : (e.returnValue = false);
         e.target.blur();
+        if (pageCache[e.target.href]) {
+          api.transition(e.target.href);
+          return;
+        }
+        api.request({
+          complete: (function (href) {
+            return function (xhr) {
+              api.cache(href, xhr.responseText);
+              api.transition(href);
+            };
+          }(e.target.href)),
+          url: e.target.href
+        });
       }
     },
     
     normalizeEvent = function (e) {
       // IE doesn't pass the event to the function grab it from the window object
       e = e || window.event;
+      var
+        parts = [
+          "hash",
+          "host",
+          "hostname",
+          "href",
+          "pathname",
+          // "port", // Safari doesn't like working with port this way...?
+          "protocol",
+          "search",
+          "target",
+          "text"
+        ];
       
       // Safari & IE support srcElement
       e.target = e.srcElement || (e.target.nodeType == 3 ? e.target.parentNode : e.target);
-      e.href = (e.srcElement || e.target).href || "";
 
+      // make sure href is a valid attribute to prevent errors
       // dont forget pathname, host, hostname, ...
+      while (parts.length) {
+        e.target[parts[0]] = (e.srcElement || e.target)[parts[0]] || "";
+        parts.shift();
+      }
       
       return e;
     },
@@ -56,23 +93,42 @@ var Intercept = (function () {
     pageCache = {},
     
     trapEvent = (function (handler) {
-      var on = handler === "attachEvent" ? "on" : "";
+      var
+        on = handler === "attachEvent" ? "on" : "";
+
       return function (element, eventType, fn) {
         element[handler](on + eventType, fn, false);
       };
     }(browserEvents.add));
     
   api.cache = function (key, obj) {
-    if (!obj) {
-      !pageCache[key] && pageCache[key] = obj;
-    } else {
-      return pageCache[key];
+    if (obj) {
+      pageCache[key] = obj;
     }
+    return pageCache[key];
   };
 
   api.config = function (userConfig) {
     config = userConfig;
-    api.cache(window.location.pathname, findChild(_(config.wrapperID), config.contentDOM))
+    api.cache(window.location.href, findChild(_(config.wrapperID), config.contentDOM).innerHTML);
+  };
+
+  api.request = function (settings) {
+    try {
+      var
+        xhr = new window.XMLHttpRequest();
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          settings.complete(xhr);
+        }
+      };
+
+      xhr.open("GET", settings.url);
+      xhr.send();
+    } catch (e) {
+      throw "Asynchronous method not provided by browser?";
+    }
   };
 
   api.start = function (config) {
@@ -83,6 +139,10 @@ var Intercept = (function () {
   
   api.stop = function () {
     dropEvent(document.body, "click", localLinks);
+  };
+
+  api.transition = function (h) {
+    _(config.wrapperID).innerHTML = pageCache[h];
   };
   
   // register what element will hold the content returned from the server
