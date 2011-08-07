dc7d998536afbd8446b24d96c1a2487327da7a8e
Joshua T Kalis <kalisjoshua@gmail.com>
2011-08-07 08:53:12 -0400

    started pageCache object to hold pages in memory

--- js/intercept.js
+++ js/intercept.js
@@ -3,12 +3,18 @@
 
 var Intercept = (function () {
   var
+    _ = function (id) {
+      return document.getElementById(id);
+    },
+
     api = {},
     
     browserEvents = {
       add: (typeof attachEvent !== "undefined") ? "attachEvent" : "addEventListener",
       rem: (typeof detachEvent !== "undefined") ? "detachEvent" : "removeEventListener"
     },
+
+    config = {},
     
     dropEvent = (function (handler) {
       var on = handler === "detachEvent" ? "on" : "";
@@ -16,24 +22,38 @@ var Intercept = (function () {
         element[handler](on + eventType, fn, false);
       };
     }(browserEvents.rem)),
+
+    findChild = function (parent, child) {
+      var
+        indx = 0,
+        nodes = parent.childNodes,
+        regex = new RegExp(child, "i");
+      while (!regex.test(nodes[indx].nodeName) && indx++ < nodes.length);
+      return nodes[indx];
+    },
     
     localLinks = function (e) {
       e = normalizeEvent(e);
-      alert(e.href);
-      // if (e.target.nodeName == "A" && window.location.host === e.target.host) {
-        // e.target.pathname
+      if (e.target.href !== "" && window.location.host === e.target.host) {
         e.preventDefault ? e.preventDefault() : (e.returnValue = false);
         e.target.blur();
-      // }
+      }
     },
     
     normalizeEvent = function (e) {
+      // IE doesn't pass the event to the function grab it from the window object
       e = e || window.event;
+      
+      // Safari & IE support srcElement
       e.target = e.srcElement || (e.target.nodeType == 3 ? e.target.parentNode : e.target);
       e.href = (e.srcElement || e.target).href || "";
+
+      // dont forget pathname, host, hostname, ...
       
       return e;
     },
+
+    pageCache = {},
     
     trapEvent = (function (handler) {
       var on = handler === "attachEvent" ? "on" : "";
@@ -42,26 +62,31 @@ var Intercept = (function () {
       };
     }(browserEvents.add));
     
-  // register what element will hold the content returned from the server
-  
-  //
-  api.start = function () {
-    
+  api.cache = function (key, obj) {
+    if (!obj) {
+      !pageCache[key] && pageCache[key] = obj;
+    } else {
+      return pageCache[key];
+    }
+  };
+
+  api.config = function (userConfig) {
+    config = userConfig;
+    api.cache(window.location.pathname, findChild(_(config.wrapperID), config.contentDOM))
+  };
+
+  api.start = function (config) {
+    api.config(config);
+
     trapEvent(document.body, "click", localLinks);
   };
   
   api.stop = function () {
-    
     dropEvent(document.body, "click", localLinks);
   };
   
+  // register what element will hold the content returned from the server
   // register what element to pull from the DOM returned by the server
-  
-  // 
-  api.trap = function (element, event, fn) {
-    trapEvent(element, event, fn);
-  };
-  
   // add event listeners to links on the page
   // request the document from the server
   // ignore links with defined rules
@@ -70,4 +95,7 @@ var Intercept = (function () {
   return api;
 }());
 
-Intercept.start();
+Intercept.start({
+  contentDOM: "section",
+  wrapperID: "content_wrapper"
+});
